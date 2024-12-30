5308263b87e350efc53f8839ff4854ef7fc221cd
muhammetenesaydin <157585657+muhammetenesaydin@users.noreply.github.com>
2024-12-30 11:10:13 +0300

    Update tek-sıralama.py

--- tek-sıralama.py
+++ tek-sıralama.py
@@ -1,3 +1,11 @@
+"""
+Size bir dizi sayı verilecek. Çift sayıları orijinal konumlarında bırakarak tek sayıları artan şekilde sıralamanız gerekir.
+
+Örnekler
+[7, 1] => [1, 7]
+[5, 8, 6, 3, 4] => [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0] => [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+"""
 
 def sort_array(src):
     odds = sorted([x for x in src if x % 2 != 0])
